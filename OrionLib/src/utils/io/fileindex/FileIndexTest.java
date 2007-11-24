997d9bde75b5ee7ad65671d6a2840f33b12f8491
royerloic <royerloic@e328a0a9-9e27-0410-8061-4940f309d3b3>
2007-11-24 12:46:17 +0000

    new FileIndex class

---
+++ OrionLib/src/utils/io/fileindex/FileIndexTest.java
@@ -0,0 +1,51 @@
+package utils.io.fileindex;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import org.junit.Test;
+
+public class FileIndexTest
+{
+	
+	@Test
+	public void testFileIndexTest() throws IOException
+	{
+		File lCurrentFolder = new File(".");
+		System.out.println(lCurrentFolder.getAbsolutePath());
+		File lFile = new File("bin/utils/io/fileindex/test.tab.txt");
+		FileIndex lFileIndex = new FileIndex(lFile,"\\t");
+		
+		lFileIndex.buildIndex(1);
+		
+		lFileIndex.open();
+		assertEquals(lFileIndex.getLine("bla")[0],"1");
+		assertEquals(lFileIndex.getLine("blu")[0],"2");
+		assertEquals(lFileIndex.getLine("blublu")[0],"3");
+		assertEquals(lFileIndex.getLine("ohoh")[0],"4");
+
+		lFileIndex.close();
+	}
+		
+	@Test
+	public void testFileIndexTestWithCache() throws IOException
+	{
+		File lCurrentFolder = new File(".");
+		System.out.println(lCurrentFolder.getAbsolutePath());
+		File lFile = new File("bin/utils/io/fileindex/test.tab.txt");
+		FileIndex lFileIndex = new FileIndex(lFile,"\\t");
+		
+		lFileIndex.buildIndex(1);
+		
+		lFileIndex.open();
+		assertEquals(lFileIndex.getLine("bla")[0],"1");
+		assertEquals(lFileIndex.getLine("blu")[0],"2");
+		assertEquals(lFileIndex.getLine("blublu")[0],"3");
+		assertEquals(lFileIndex.getLine("ohoh")[0],"4");
+
+		lFileIndex.close();
+	}
+
+}
