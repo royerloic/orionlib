ce8f86a63c5c34b047419fcafa31473965c02da9
royerloic <royerloic@e328a0a9-9e27-0410-8061-4940f309d3b3>
2007-11-24 13:02:52 +0000

    edded cache to FileIndex

--- OrionLib/src/utils/io/fileindex/FileIndex.java
+++ OrionLib/src/utils/io/fileindex/FileIndex.java
@@ -6,21 +6,24 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.io.Serializable;
+import java.lang.ref.SoftReference;
 import java.util.HashMap;
 import java.util.regex.Pattern;
 
-public class FileIndex
+public class FileIndex implements Serializable
 {
-	private final File mFile;
-	private final String	mColumnSeparatorRegex;
-	private final Pattern mColumnSeparatorPattern;
-	private HashMap<String,Long> mIndexMap = new HashMap<String,Long>();
-	
-	private File IndexFile;
-	private RandomAccessFile	mRandomAccessFile;
+	private final File																mFile;
+	private final String															mColumnSeparatorRegex;
+	private final Pattern															mColumnSeparatorPattern;
+	private HashMap<String, Long>											mIndexMap	= new HashMap<String, Long>();
 
-	
+	private File																			IndexFile;
+	private RandomAccessFile													mRandomAccessFile;
 
+	private HashMap<String, SoftReference<String[]>>	mCacheMap	= new HashMap<String, SoftReference<String[]>>();
+	
+	
 	public FileIndex(File pFile, String pColumnSeparatorRegex)
 	{
 		super();
@@ -28,7 +31,7 @@ public class FileIndex
 		mColumnSeparatorRegex = pColumnSeparatorRegex;
 		mColumnSeparatorPattern = Pattern.compile(mColumnSeparatorRegex);
 	}
-	
+
 	public void buildIndex(final int pColumnIndex) throws IOException
 	{
 		final String lineSep = System.getProperty("line.separator");
@@ -38,36 +41,48 @@ public class FileIndex
 		long lPosition = 0;
 		while ((nextLine = lBufferedReader.readLine()) != null)
 		{
-			final String[] lColumnArray = mColumnSeparatorPattern.split(nextLine,-1);
-			if(lColumnArray.length>pColumnIndex)
+			final String[] lColumnArray = mColumnSeparatorPattern.split(nextLine, -1);
+			if (lColumnArray.length > pColumnIndex)
 			{
 				final String lIndexColumnValue = lColumnArray[pColumnIndex];
 				mIndexMap.put(lIndexColumnValue, lPosition);
 				System.out.println(lIndexColumnValue);
 			}
-			lPosition += nextLine.length()+lineSep.length();
-		}		
+			lPosition += nextLine.length() + lineSep.length();
+		}
+		if(mFile.length()!=lPosition)
+			throw new RuntimeException("Line Separator of wrong size -> index corrupt");
 		lBufferedReader.close();
 	}
-	
+
 	public final void open() throws FileNotFoundException
 	{
-		mRandomAccessFile = new RandomAccessFile(mFile,"r");
+		mRandomAccessFile = new RandomAccessFile(mFile, "r");
 	}
-	
+
 	public final void close() throws IOException
 	{
 		mRandomAccessFile.close();
 	}
-	
+
 	public final String[] getLine(final String pValue) throws IOException
 	{
-		final long lLinePosition = mIndexMap.get(pValue);	
+		final long lLinePosition = mIndexMap.get(pValue);
 		mRandomAccessFile.seek(lLinePosition);
 		final String lLine = mRandomAccessFile.readLine();
-		final String[] lColumnArray = mColumnSeparatorPattern.split(lLine,-1);
+		final String[] lColumnArray = mColumnSeparatorPattern.split(lLine, -1);
 		return lColumnArray;
 	}
-	
-	
+
+	public final String[] getLineAndCache(final String pValue) throws IOException
+	{
+		SoftReference<String[]> lSoftRef = mCacheMap.get(pValue);
+		if(lSoftRef==null || lSoftRef.get()==null)
+		{
+			final String[] lColumnArray = getLine(pValue);
+			lSoftRef = new SoftReference<String[]>(lColumnArray);
+		}		
+		return lSoftRef.get();
+	}
+
 }
