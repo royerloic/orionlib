c67e2e972a10e8927db3e4edb8f0f7511ecc4673
royerloicwork <royerloicwork@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-01-21 10:47:11 +0000

    fixed problems when list is empty

--- trunk/OrionLib/src/utils/structures/RingList.java
+++ trunk/OrionLib/src/utils/structures/RingList.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class RingList<O> 
+public class RingList<O>
 {
 	List<O> mList = Collections.emptyList();
-		
+
 	int mCursor = 0;
-	
+
 	public RingList(List<O> pList)
 	{
 		super();
 		mList = pList;
 	}
-	
+
 	public RingList(O[] pValues)
 	{
 		super();
@@ -25,43 +25,62 @@ public class RingList<O>
 			mList.add(lO);
 		}
 	}
-	
+
 	public RingList()
 	{
 	}
 
 	public void setList(List<O> pList)
 	{
-		mList=pList;
+		mList = pList;
 	}
 
 	public void resetCursor()
 	{
-		mCursor=0;
+		mCursor = 0;
 	}
-	
+
 	public O next()
 	{
-		mCursor = (mCursor+1)%mList.size();
-		return mList.get(mCursor);
+		if (!mList.isEmpty())
+		{
+			mCursor = (mCursor + 1) % mList.size();
+			return mList.get(mCursor);
+		}
+		else
+		{
+			return null;
+		}
 	}
-	
+
 	public O previous()
 	{
-		if(mCursor==0)
-			mCursor=mList.size()-1;
-		else
-			mCursor--;
+		if (!mList.isEmpty())
+		{
+			if (mCursor == 0)
+				mCursor = mList.size() - 1;
+			else
+				mCursor--;
 
-		return mList.get(mCursor);
+			return mList.get(mCursor);
+		}
+		else
+		{
+			return null;
+		}
 	}
-	
+
 	public O get()
 	{
-		return mList.get(mCursor);
+		if (!mList.isEmpty())
+		{
+			return mList.get(mCursor);
+		}
+		else
+		{
+			return null;
+		}
 	}
-
-
 
 	@Override
 	public int hashCode()
@@ -76,24 +95,37 @@ public class RingList<O>
 	@Override
 	public boolean equals(Object obj)
 	{
-		if (this == obj) return true;
-		if (obj == null) return false;
-		if (getClass() != obj.getClass()) return false;
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
 		final RingList other = (RingList) obj;
-		if (mCursor != other.mCursor) return false;
+		if (mCursor != other.mCursor)
+			return false;
 		if (mList == null)
 		{
-			if (other.mList != null) return false;
+			if (other.mList != null)
+				return false;
 		}
-		else if (!mList.equals(other.mList)) return false;
+		else if (!mList.equals(other.mList))
+			return false;
 		return true;
 	}
 
 	@Override
 	public String toString()
 	{
-		return get().toString();
+		O lO = get();
+		if (lO != null)
+		{
+			return get().toString();
+		}
+		else
+		{
+			return null;
+		}
 	}
-	
-	
+
 }
