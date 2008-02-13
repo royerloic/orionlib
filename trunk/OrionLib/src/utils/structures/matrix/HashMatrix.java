a41a5e4aceb03e71047e73708f2a2dfd4d0c5203
royerloicwork <royerloicwork@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-02-13 14:47:19 +0000

    lr

---
+++ trunk/OrionLib/src/utils/structures/matrix/HashMatrix.java
@@ -0,0 +1,18 @@
+package utils.structures.matrix;
+
+import utils.structures.map.HashMapMap;
+
+public class HashMatrix<V> extends HashMapMap<Integer, Integer, V>
+{
+
+	public void put(final int pX, final int pY, final V pV)
+	{
+		super.put(pX, pY, pV);
+	}
+
+	public V get(final int pX, final int pY)
+	{
+		return super.get(pX, pY);
+	}
+
+}
---
+++ trunk/OrionLib/src/utils/structures/matrix/Matrix.java
@@ -0,0 +1,8 @@
+package utils.structures.matrix;
+
+import java.util.List;
+
+public interface Matrix<O> extends List<List<O>>
+{
+
+}
