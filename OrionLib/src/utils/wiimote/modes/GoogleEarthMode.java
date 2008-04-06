e07fc0d5e8792c770faef5e0f72deeb4187a3542
royerloic <royerloic@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-04-06 12:39:57 +0000

    lr

--- OrionLib/src/utils/wiimote/modes/GoogleEarthMode.java
+++ OrionLib/src/utils/wiimote/modes/GoogleEarthMode.java
@@ -65,91 +65,79 @@ public class GoogleEarthMode extends WiiRemoteAdapter implements WiiMode
 	HashSet<Integer> mVerticalEventSet = new HashSet<Integer>();
 	HashSet<Integer> mHoryzontalEventSet = new HashSet<Integer>();
 
-	double xdd=0;
-	double zdd=0;
-	
+	double xdd = 0;
+	double zdd = 0;
+
 	public void accelerationInputReceived(WRAccelerationEvent pEvent)
 	{
-		final double epsilon = 0.15;
+		final double epsilon = 0.05;
+		final double zeta = 0.15;
 		final double zddoffset = 0.2;
-		xdd = xdd*0.2 + pEvent.getXAcceleration()*0.8;
-		zdd = zdd*0.2 + (pEvent.getZAcceleration() - zddoffset) *0.8;
-		
+		xdd = xdd * 0.2 + pEvent.getXAcceleration() * 0.8;
+		zdd = zdd * 0.2 + (pEvent.getZAcceleration() - zddoffset) * 0.8;
+
 		System.out.println("xdd=" + xdd);
 		System.out.println("zdd=" + zdd);
 
-		if (zdd > epsilon)
+		if (isBpressed)
 		{
-			if (!mVerticalEventSet.contains(KeyEvent.VK_UP))
+			if (zdd > epsilon && Math.abs(xdd) < zeta )
 			{
-				mRobot.keyPress(KeyEvent.VK_UP);
-				mVerticalEventSet.add(KeyEvent.VK_UP);
-			}
-		}
-		else if (zdd < -epsilon )
-		{
-			if (!mVerticalEventSet.contains(KeyEvent.VK_DOWN))
-			{
-				mRobot.keyPress(KeyEvent.VK_DOWN);
-				mVerticalEventSet.add(KeyEvent.VK_DOWN);
-			}
-		}
-		else if(Math.abs(zdd)<epsilon)
-		{
-			for (Integer key : mVerticalEventSet)
-			{
-				mRobot.keyRelease(key);
-			}
-			mVerticalEventSet.clear();
-		}
-		
-		if (xdd > epsilon)
-		{
-			if (!mHoryzontalEventSet.contains(KeyEvent.VK_RIGHT))
-			{
-				mRobot.keyPress(KeyEvent.VK_RIGHT);
-				mHoryzontalEventSet.add(KeyEvent.VK_RIGHT);
-			}
-		}
-		else if (xdd < -epsilon )
-		{
-			if (!mHoryzontalEventSet.contains(KeyEvent.VK_LEFT))
-			{
-				mRobot.keyPress(KeyEvent.VK_LEFT);
-				mHoryzontalEventSet.add(KeyEvent.VK_LEFT);
-			}
-		}
-		else if(Math.abs(xdd)<epsilon)
-		{
-			for (Integer key : mHoryzontalEventSet)
-			{
-				mRobot.keyRelease(key);
-			}
-			mHoryzontalEventSet.clear();
-		}
 
-		/*if (xdd > epsilon)
-		{
-			xcounter += xdd;
-			if (xcounter > 1)
-			{
-				mRobot.keyPress(KeyEvent.VK_RIGHT);
-				//if (xcounter < 2)					mRobot.keyRelease(KeyEvent.VK_RIGHT);
-				xcounter--;
+				zcounter += zdd;
+				if (zcounter > 1)
+				{
+					zcounter--;
+					mRobot.mouseWheel(-1);
+				}
 			}
-		}
-		else if (xdd < -epsilon)
-		{
-			xcounter += xdd;
-			if (xcounter < -1)
+			else if (zdd < -epsilon && Math.abs(xdd) < zeta)
 			{
-				mRobot.keyPress(KeyEvent.VK_LEFT);
-				//if (xcounter > -2)					mRobot.keyRelease(KeyEvent.VK_LEFT);
-				xcounter++;
+
+				zcounter += zdd;
+				if (zcounter < -1)
+				{
+					zcounter++;
+					mRobot.mouseWheel(+1);
+				}
+			}
+
+			if (xdd > epsilon && Math.abs(zdd) < zeta)
+			{
+
+				xcounter += xdd;
+				if (xcounter > 1)
+				{
+					xcounter--;
+					mRobot.keyPress(KeyEvent.VK_CONTROL);
+					mRobot.mouseWheel(-1);
+					mRobot.keyRelease(KeyEvent.VK_CONTROL);
+				}
+
+			}
+			else if (xdd < -epsilon && Math.abs(zdd) < zeta)
+			{
+
+				xcounter += xdd;
+				if (xcounter < -1)
+				{
+					xcounter++;
+					mRobot.keyPress(KeyEvent.VK_CONTROL);
+					mRobot.mouseWheel(+1);
+					mRobot.keyRelease(KeyEvent.VK_CONTROL);
+				}
 			}
 		}
 
 		/***************************************************************************
+		 * if (xdd > epsilon) { xcounter += xdd; if (xcounter > 1) {
+		 * mRobot.keyPress(KeyEvent.VK_RIGHT); //if (xcounter < 2)
+		 * mRobot.keyRelease(KeyEvent.VK_RIGHT); xcounter--; } } else if (xdd <
+		 * -epsilon) { xcounter += xdd; if (xcounter < -1) {
+		 * mRobot.keyPress(KeyEvent.VK_LEFT); //if (xcounter > -2)
+		 * mRobot.keyRelease(KeyEvent.VK_LEFT); xcounter++; } }
+		 * 
+		 * /***************************************************************************
 		 * if (Math.abs(zdd)<epsilon) { for(Integer lInteger :
 		 * mHoryzontalEventList) { mRobot.keyRelease(lInteger); }
 		 * mHoryzontalEventList.clear(); } else if (xdd>epsilon) {
@@ -160,22 +148,84 @@ public class GoogleEarthMode extends WiiRemoteAdapter implements WiiMode
 		 **************************************************************************/
 	}
 
+	boolean isApressed = false;
+	boolean isBpressed = false;
+
+	boolean isDownPressed = false;
+	boolean isUpPressed = false;
+	boolean isRightPressed = false;
+	boolean isLeftPressed = false;
+
+	boolean isMinusPressed = false;
+	boolean isPlusPressed = false;
+
 	public void buttonInputReceived(WRButtonEvent pEvent)
 	{
-		/***************************************************************************
-		 * if (!isDownPressed && (pEvent.wasPressed(WRButtonEvent.DOWN) ||
-		 * pEvent.wasPressed(WRButtonEvent.A))) { isDownPressed = true;
-		 * mRobot.keyPress(KeyEvent.VK_DOWN); } else if (!isUpPressed &&
-		 * (pEvent.wasPressed(WRButtonEvent.UP) ||
-		 * pEvent.wasPressed(WRButtonEvent.B))) { isUpPressed = true;
-		 * mRobot.keyPress(KeyEvent.VK_UP); } else if (isDownPressed &&
-		 * (pEvent.wasReleased(WRButtonEvent.DOWN) ||
-		 * pEvent.wasReleased(WRButtonEvent.A))) { isDownPressed = false;
-		 * mRobot.keyRelease(KeyEvent.VK_DOWN); } else if (isUpPressed &&
-		 * (pEvent.wasReleased(WRButtonEvent.UP) ||
-		 * pEvent.wasReleased(WRButtonEvent.B))) { isUpPressed = false;
-		 * mRobot.keyRelease(KeyEvent.VK_UP); } /
-		 **************************************************************************/
-	}
+		isApressed = pEvent.isPressed(WRButtonEvent.A);
+		isBpressed = pEvent.isPressed(WRButtonEvent.B);
 
+		if (!isDownPressed && (pEvent.wasPressed(WRButtonEvent.DOWN)))
+		{
+			isDownPressed = true;
+			mRobot.keyPress(KeyEvent.VK_DOWN);
+		}
+		else if (!isUpPressed && (pEvent.wasPressed(WRButtonEvent.UP)))
+		{
+			isUpPressed = true;
+			mRobot.keyPress(KeyEvent.VK_UP);
+		}
+		else if (isDownPressed && (pEvent.wasReleased(WRButtonEvent.DOWN)))
+		{
+			isDownPressed = false;
+			mRobot.keyRelease(KeyEvent.VK_DOWN);
+		}
+		else if (isUpPressed && (pEvent.wasReleased(WRButtonEvent.UP)))
+		{
+			isUpPressed = false;
+			mRobot.keyRelease(KeyEvent.VK_UP);
+		}
+
+		if (!isRightPressed && (pEvent.wasPressed(WRButtonEvent.RIGHT)))
+		{
+			isRightPressed = true;
+			mRobot.keyPress(KeyEvent.VK_RIGHT);
+		}
+		else if (!isLeftPressed && (pEvent.wasPressed(WRButtonEvent.LEFT)))
+		{
+			isLeftPressed = true;
+			mRobot.keyPress(KeyEvent.VK_LEFT);
+		}
+		else if (isRightPressed && (pEvent.wasReleased(WRButtonEvent.RIGHT)))
+		{
+			isRightPressed = false;
+			mRobot.keyRelease(KeyEvent.VK_RIGHT);
+		}
+		else if (isLeftPressed && (pEvent.wasReleased(WRButtonEvent.LEFT)))
+		{
+			isLeftPressed = false;
+			mRobot.keyRelease(KeyEvent.VK_LEFT);
+		}
+
+		if (!isMinusPressed && (pEvent.wasPressed(WRButtonEvent.MINUS)))
+		{
+			isMinusPressed = true;
+			mRobot.keyPress(KeyEvent.VK_PAGE_DOWN);
+		}
+		else if (!isPlusPressed && (pEvent.wasPressed(WRButtonEvent.PLUS)))
+		{
+			isPlusPressed = true;
+			mRobot.keyPress(KeyEvent.VK_PAGE_UP);
+		}
+		else if (isMinusPressed && (pEvent.wasReleased(WRButtonEvent.MINUS)))
+		{
+			isMinusPressed = false;
+			mRobot.keyRelease(KeyEvent.VK_PAGE_DOWN);
+		}
+		else if (isPlusPressed && (pEvent.wasReleased(WRButtonEvent.PLUS)))
+		{
+			isPlusPressed = false;
+			mRobot.keyRelease(KeyEvent.VK_PAGE_UP);
+		}
+
+	}
 }
