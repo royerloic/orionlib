3714976bb1885435326223ef3709c79fb39be74a
royerloic <royerloic@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-04-28 22:21:34 +0000

    lr

---
+++ OrionLib/src/utils/structures/fast/set/FastIntegerSetTests.java
@@ -0,0 +1,17 @@
+package utils.structures.fast.set;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ */
+public class FastIntegerSetTests
+{
+  
+}
