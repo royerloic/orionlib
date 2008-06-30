583572e9763d6ea156d24d338c2fdec4db610401
royerloicwork <royerloicwork@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-06-30 21:24:11 +0000

    lr

--- OrionLib/src/utils/graphics/BufferingFrame.java
+++ OrionLib/src/utils/graphics/BufferingFrame.java
@@ -1,29 +1,56 @@
 package utils.graphics;
 
+import java.awt.AWTException;
+import java.awt.BufferCapabilities;
+import java.awt.Canvas;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
 import java.awt.HeadlessException;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.image.BufferStrategy;
 import java.awt.image.VolatileImage;
 
-public class BufferingFrame extends Frame implements GraphicsProvider
+import javax.swing.JFrame;
+
+public class BufferingFrame extends JFrame implements GraphicsProvider
 {
 
 	private static final long serialVersionUID = 1L;
 	private Graphics2D mDrawGraphics;
+	private BufferStrategy mBufferStrategy;
 
 	public BufferingFrame(String pTitle)
 	{
 		super(pTitle);
 		this.setSize(800, 600);
 		this.setVisible(true);
+
+		this.setIgnoreRepaint(true);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsConfiguration gc = ge	.getDefaultScreenDevice()
+																	.getDefaultConfiguration();
+
 		this.createBufferStrategy(2);
+		mBufferStrategy = this.getBufferStrategy();
+		System.out.println("getBackBufferCapabilities().isAccelerated()" + String.valueOf(mBufferStrategy	.getCapabilities()
+																																																			.getBackBufferCapabilities()
+																																																			.isAccelerated()));
+		System.out.println("getFrontBufferCapabilities().isAccelerated()" + String.valueOf(mBufferStrategy.getCapabilities()
+																																																			.getFrontBufferCapabilities()
+																																																			.isAccelerated()));
+		System.out.println("getBackBufferCapabilities().isTrueVolatile()" + String.valueOf(mBufferStrategy.getCapabilities()
+																																																			.getBackBufferCapabilities()
+																																																			.isTrueVolatile()));
+		System.out.println("getFrontBufferCapabilities().isTrueVolatile()" + String.valueOf(mBufferStrategy	.getCapabilities()
+																																																				.getFrontBufferCapabilities()
+																																																				.isTrueVolatile()));
+
 	}
 
 	public boolean isDecorated()
@@ -38,16 +65,24 @@ public class BufferingFrame extends Frame implements GraphicsProvider
 
 	public Graphics2D getDrawGraphics()
 	{
-		BufferStrategy lBufferStrategy = this.getBufferStrategy();
-		mDrawGraphics = (Graphics2D) lBufferStrategy.getDrawGraphics();
+		try
+		{
+			mDrawGraphics = (Graphics2D) mBufferStrategy.getDrawGraphics();
+		}
+		catch (java.lang.IllegalStateException e)
+		{
+			System.out.println("Switched screen, buffer configuration not valid anymore:");
+			System.out.println(e.getMessage());
+			this.createBufferStrategy(2);
+			mBufferStrategy = this.getBufferStrategy();
+		}
 		return mDrawGraphics;
 	}
 
 	public void showGraphics()
 	{
-		BufferStrategy lBufferStrategy = this.getBufferStrategy();
-		lBufferStrategy.show();		
-		Toolkit.getDefaultToolkit().sync();	
+		mBufferStrategy.show();
+		Toolkit.getDefaultToolkit().sync();
 		mDrawGraphics.dispose();
 	}
 }
