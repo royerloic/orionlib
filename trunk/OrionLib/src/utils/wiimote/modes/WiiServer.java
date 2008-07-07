1bb8d8bc824c258e5e1cf0ce6fad190cdcf98aab
royerloic <royerloic@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-07-07 09:33:54 +0000

    lr

---
+++ trunk/OrionLib/src/utils/wiimote/modes/WiiServer.java
@@ -0,0 +1,176 @@
+package utils.wiimote.modes;
+
+import java.awt.CheckboxMenuItem;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+import java.net.SocketException;
+
+import wiiremotej.IRLight;
+import wiiremotej.WiiRemote;
+import wiiremotej.event.WRIREvent;
+import wiiremotej.event.WiiRemoteAdapter;
+
+public class WiiServer extends WiiRemoteAdapter implements Runnable, WiiMode
+{
+
+	public static final int sWiiServerPort = 1234;
+
+	CheckboxMenuItem mSlideShowModeItem = new CheckboxMenuItem(	"Server Mode",
+																															false);
+
+	boolean mActive = false;
+
+	double[] mX = new double[4];
+	double[] mY = new double[4];
+
+	private Thread mThread;
+	private boolean mStopSignal = false;
+
+	private int mCount;
+
+	public WiiServer()
+	{
+		super();
+	}
+
+	public void activate(WiiRemote pRemote)
+	{
+		try
+		{
+			pRemote.addWiiRemoteListener(this);
+			pRemote.setAccelerometerEnabled(false);
+			pRemote.setIRSensorEnabled(true, WRIREvent.BASIC);
+		}
+		catch (final Throwable e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		mStopSignal = false;
+		mThread = new Thread(this);
+		mThread.setDaemon(true);
+		mThread.start();
+
+	}
+
+	public void deactivate(WiiRemote pRemote)
+	{
+		try
+		{
+			pRemote.removeWiiRemoteListener(this);
+			pRemote.setIRSensorEnabled(false, WRIREvent.BASIC);
+		}
+		catch (final Throwable e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		mStopSignal = true;
+		mThread=null;
+	}
+
+	public CheckboxMenuItem getMenuItem()
+	{
+		return mSlideShowModeItem;
+	}
+
+	@SuppressWarnings("deprecation")
+	public void run()
+	{
+
+		DatagramSocket aSocket = null;
+
+		try
+		{
+			aSocket = new DatagramSocket(sWiiServerPort);
+			aSocket.setSoTimeout(1);
+			byte[] bufferin = new byte[100];
+			DatagramPacket packetin = new DatagramPacket(bufferin, bufferin.length);
+			byte[] bufferout = new byte[100];
+			DatagramPacket packetout = new DatagramPacket(bufferout, bufferout.length);
+
+			StringBuilder lStringBuilderOut = new StringBuilder();
+
+			while (!mStopSignal)
+			{
+
+				try
+				{
+					aSocket.receive(packetin);
+					
+					String lDataReceived = new String(packetin.getData(),
+																						0,
+																						packetin.getLength());
+					if (lDataReceived.equals("start"))
+					{
+						mActive = true;
+					}
+					else if (lDataReceived.equals("stop"))
+					{
+						mActive = false;
+					}
+				}
+				catch (IOException e)
+				{
+					
+				}
+
+
+				if (mActive)
+				{
+					lStringBuilderOut.setLength(0);
+					for (int i = 0; i < mCount; i++)
+					{
+						lStringBuilderOut.append(" " + mX[i] + ", " + mY[i] + " ;");
+					}
+					String lDataString = lStringBuilderOut.toString();
+					byte[] lBytes = lDataString.getBytes();
+					packetout.setData(lBytes, 0, lBytes.length);
+
+					packetout.setSocketAddress(packetin.getSocketAddress());
+					packetout.setPort(packetin.getPort());
+					aSocket.send(packetout);
+				}
+			}
+		}
+		catch (SocketException e)
+		{
+			System.out.println("Socket: " + e.getMessage());
+		}
+		catch (IOException e)
+		{
+			System.out.println("IO: " + e.getMessage());
+		}
+		finally
+		{
+			if (aSocket != null)
+				aSocket.close();
+		}
+
+	}
+
+	public void IRInputReceived(final WRIREvent pWRIREvent)
+	{
+		int i = 0;
+		for (final IRLight light : pWRIREvent.getIRLights())
+		{
+			if (light != null)
+			{
+				synchronized (this)
+				{
+					mX[i] = light.getX();
+					mY[i] = light.getY();
+				}
+				i++;
+			}
+		}
+
+		mCount = i;
+
+	}
+
+}
---
+++ trunk/OrionLib/src/utils/wiimote/modes/test/WiiServerTest.java
@@ -0,0 +1,71 @@
+package utils.wiimote.modes.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+import org.junit.Test;
+
+import utils.structures.set.FirstInPrioritizedSetDecomposer;
+import utils.wiimote.modes.WiiServer;
+import utils.wiimote.modes.WiiServerClient;
+
+public class WiiServerTest
+{
+
+	@Test
+	public void testWiiServerClient() throws IOException, InterruptedException
+	{
+		WiiServerClient lWiiServerClient = new WiiServerClient();
+		lWiiServerClient.start();
+		Thread.sleep(100000);
+		lWiiServerClient.stop();
+		
+	}
+	
+	@Test
+	public void testWiiServer() throws IOException
+	{
+		DatagramSocket aSocket = null;
+
+		try
+		{
+			aSocket = new DatagramSocket(2345);
+			
+			byte[] lBytesOut = "start".getBytes();
+			DatagramPacket packetout = new DatagramPacket(lBytesOut,lBytesOut.length);
+			packetout.setAddress(InetAddress.getLocalHost());
+			packetout.setPort(WiiServer.sWiiServerPort);
+			aSocket.send(packetout);
+			
+			byte[] bufferin = new byte[100];
+			DatagramPacket packetin = new DatagramPacket(bufferin, bufferin.length);
+			for(int i=0; i<1000000000; i++)
+			{
+				System.out.println("waiting...");
+				aSocket.receive(packetin);
+				String lReceived = new String(packetin.getData(),0,packetin.getLength());
+				System.out.println(lReceived);
+			}
+			
+		}
+		catch (SocketException e)
+		{
+			System.out.println("Socket: " + e.getMessage());
+		}
+		catch (IOException e)
+		{
+			System.out.println("IO: " + e.getMessage());
+		}
+		finally
+		{
+			if (aSocket != null)
+				aSocket.close();
+		}
+	}
+
+}
