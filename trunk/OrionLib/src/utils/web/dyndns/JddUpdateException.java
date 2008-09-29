95940094012eb30c523436737d8d105c638a3a21
royerloicwork <royerloicwork@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-09-29 08:04:27 +0000

    lr

---
+++ trunk/OrionLib/src/utils/web/dyndns/JddUpdateException.java
@@ -0,0 +1,23 @@
+/*
+ * Created by IntelliJ IDEA.
+ * User: Joe
+ * Date: Jun 28, 2002
+ * Time: 11:47:44 AM
+ * To change template for new class use 
+ * Code Style | Class Templates options (Tools | IDE Options).
+ */
+package utils.web.dyndns;
+
+public class JddUpdateException extends Exception{
+  public JddUpdateException(String message, Exception e) {
+    super(message, e);
+  }
+
+  public JddUpdateException(String message) {
+    super(message);
+  }
+
+  public JddUpdateException(Exception e) {
+    super(e);
+  }
+}
