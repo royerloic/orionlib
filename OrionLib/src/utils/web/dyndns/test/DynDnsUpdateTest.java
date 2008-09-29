eb3ad3df74e33f19608d65d21059ba2e8acef8ed
royerloicwork <royerloicwork@e328a0a9-9e27-0410-8061-4940f309d3b3>
2008-09-29 08:04:49 +0000

    lr

---
+++ OrionLib/src/utils/web/dyndns/test/DynDnsUpdateTest.java
@@ -0,0 +1,105 @@
+package utils.web.dyndns.test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+import utils.web.dyndns.DynDnsUpdater;
+import utils.web.dyndns.JddUpdateException;
+
+public class DynDnsUpdateTest
+{
+	//@Test
+	public void testDynDnsUpdate() throws InterruptedException
+	{
+		Random rnd = new Random();
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String login = sc.nextLine();
+    String password = sc.nextLine();
+		String host = sc.nextLine();
+		String ip = "192.192.192."+Math.abs(rnd.nextInt()%255);
+		System.out.println("ip is: "+ip);
+		
+		DynDnsUpdater updater = new DynDnsUpdater("", login, password);
+		try
+		{
+			updater.update(host, ip);
+		}
+		catch (JddUpdateException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+		
+		Thread.sleep(10000);
+		
+		InetAddress inet;
+		try
+		{
+			inet = InetAddress.getByName(host);
+			System.out.println ("IP  : " + inet.getHostAddress());
+			assertEquals(ip, inet.getHostAddress());
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Test
+	public void testDynDnsUpdateAutoIp() throws InterruptedException
+	{
+		Random rnd = new Random();
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String login = sc.nextLine();
+    String password = sc.nextLine();
+		String host = sc.nextLine();
+
+		
+		DynDnsUpdater updater = new DynDnsUpdater("", login, password);
+		try
+		{
+			updater.update(host);
+		}
+		catch (JddUpdateException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	//@Test
+	public void testGetIp() throws InterruptedException
+	{
+		Random rnd = new Random();
+		
+		
+		Scanner sc = new Scanner(System.in);
+		String host = sc.nextLine();
+		InetAddress inet;
+		try
+		{
+			inet = InetAddress.getByName(host);
+			System.out.println ("IP  : " + inet.getHostAddress());
+		}
+		catch (UnknownHostException e)
+		{
+			e.printStackTrace();
+			fail();
+		}
+
+	}
+
+}
